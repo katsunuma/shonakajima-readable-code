aefa8c10f74a707f273229c77a1388dd9a09e208
shonakajima <65323438+shonakajima@users.noreply.github.com>
2022-11-02 12:16:51 +0900

    spec5:各単語にIDを割り振り、IDと単語を同時に表示するよう変更

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,9 @@
 path = "dictionary-data.txt"
 
 with open(path,encoding="utf-8") as f:
-    text = f.read()
-    print(text)
+    text = [s.strip() for s in f.readlines()]
+
+length = len(text)
+
+for i in range(length):
+    print("{}:{}".format(i+1,text[i]))
