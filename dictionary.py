77babd4442235e560c1e763e844503fcc7c16f8f
shonakajima <65323438+shonakajima@users.noreply.github.com>
2022-11-02 12:54:16 +0900

    spec5:コマンドライン引数を追加、ファイルパスを引数にして起動できるよう修正

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,21 @@
-path = "dictionary-data.txt"
+import argparse
 
-with open(path,encoding="utf-8") as f:
-    text = [s.strip() for s in f.readlines()]
+## コマンドライン引数の設定
+parser = argparse.ArgumentParser( )
+parser.add_argument("path")
+args = parser.parse_args( )
 
-length = len(text)
+def dictionary(path):
+    
+    ##データの読み込み
+    with open(path,encoding="utf-8") as f:
+        text = [s.strip() for s in f.readlines()]
+    
+    ##行数の取得
+    length = len(text)
+    
+    ##行数をIDとして表示
+    for i in range(length):
+        print("{}:{}".format(i+1,text[i]))
 
-for i in range(length):
-    print("{}:{}".format(i+1,text[i]))
+dictionary(args.path)
